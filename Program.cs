e32c462c711670aab2dbc79e32f54bdc1e472496
Corey <coreygraham@live.co.uk>
2023-10-23 22:46:16 +0100

    Create Number Generator

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NumberGenerator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Generate random number between 1 and 50
+        Random rng = new();
+        List<int> mainArray = new(25);
+        int[] numbers = new int[25];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int num = rng.Next(1,51);
+                while(numbers.Contains(num)) {
+                    num = rng.Next(1, 51);
+                }
+                numbers[i] = num;
+                
+            }
+
+            int counter = 1;
+            for (int i = 0; i < numbers.Length; i++) {
+                System.Console.Write($"|{numbers[i]}| ");
+                counter++;
+            }
+    }
+}
